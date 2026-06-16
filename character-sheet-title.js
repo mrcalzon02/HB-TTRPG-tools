66bc32f2942bc64e7f8c5e1d8690442a14cb232e
mrcalzon02 <mrcalzon02@yahoo.com>
2026-06-16 15:09:47 -0800

    Load Arcane Academic Studies launcher

--- character-sheet-title.js
+++ character-sheet-title.js
@@ -52,6 +52,7 @@
   function loadSupplementalGenerators() {
     loadScriptOnce('spell-creator-entry.js', 'data-spell-creator-entry', 'spellCreatorEntry');
     loadScriptOnce('eccentric-spell-entry.js', 'data-eccentric-spell-entry', 'eccentricSpellEntry');
+    loadScriptOnce('arcane-academic-entry.js', 'data-arcane-academic-entry', 'arcaneAcademicEntry');
     loadScriptOnce('kaysender-npc-generator.js', 'data-npc-generator', 'npcGenerator');
     loadScriptOnce('kaysender-crafting-generator.js', 'data-crafting-generator', 'craftingGenerator');
   }
