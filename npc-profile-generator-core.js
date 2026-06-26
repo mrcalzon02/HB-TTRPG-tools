77ff3c9fa7ffa615aa45d76b4680e18ec0b9d44b
mrcalzon02 <mrcalzon02@yahoo.com>
2026-06-25 18:42:40 -0800

    Connect deterministic NPC generator modules

--- npc-profile-generator-core.js
+++ npc-profile-generator-core.js
@@ -1,3 +1,21 @@
 (() => {
   'use strict';
+  const foundation = globalThis.NpcProfileGeneratorFoundation;
+  const assembly = globalThis.NpcProfileGeneratorAssembly;
+  if (!foundation || !assembly) throw new Error('NPC generator foundation and composition modules must load first.');
+  globalThis.NpcProfileGeneratorCore = Object.freeze({
+    GENERATOR_ID: assembly.GENERATOR_ID,
+    VERSION: assembly.VERSION,
+    CANONICAL_SECTIONS: foundation.CANONICAL_SECTIONS,
+    decodePointer: foundation.decodePointer,
+    pointerGet: foundation.pointerGet,
+    pointerSet: foundation.pointerSet,
+    tableEntries: foundation.tableEntries,
+    chooseTable: foundation.chooseTable,
+    generatedProfileId: foundation.generatedProfileId,
+    generateField: foundation.generateField,
+    generateFields: foundation.generateFields,
+    applyLocks: assembly.applyLocks,
+    generateProfile: assembly.generateProfile
+  });
 })();
