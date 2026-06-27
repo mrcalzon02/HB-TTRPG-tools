54a48f2d9f9bf5ad20ed1c7631fc013d9f354a3c
mrcalzon02 <mrcalzon02@yahoo.com>
2026-06-26 16:53:53 -0800

    Load World of Darkness map workspace

--- character-sheet-title.js
+++ character-sheet-title.js
@@ -63,6 +63,7 @@
     loadScriptOnce('elemental-realms-entry.js','data-elemental-realms-entry','elementalRealmsEntry');
     loadScriptOnce('solanum-umbra-entry.js','data-solanum-umbra-entry','solanumUmbraEntry');
     loadScriptOnce('world-of-darkness-entry.js','data-world-of-darkness-entry','worldOfDarknessEntry');
+    loadScriptOnce('world-of-darkness-map-workspace.js','data-world-of-darkness-map-workspace','worldOfDarknessMapWorkspace');
     loadScriptOnce('shadowrun-entry.js','data-shadowrun-entry','shadowrunEntry');
     loadScriptOnce('npc-profile-generator-entry.js','data-npc-profile-generator-entry','npcProfileGeneratorEntry');
     loadScriptOnce('kaysender-npc-generator.js','data-npc-generator','npcGenerator');
