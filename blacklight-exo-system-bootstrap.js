5dd0d77b976df694d5aea1cb606f30e059944251
mrcalzon02 <mrcalzon02@yahoo.com>
2026-07-16 18:18:04 -0800

    Load EXO imagery definitions before renderer

--- blacklight-exo-system-bootstrap.js
+++ blacklight-exo-system-bootstrap.js
@@ -34,6 +34,7 @@
       await loadStyle('blacklight-exo-orbital-layout.css');
       await loadStyle('blacklight-exo-campaign-facilities.css');
       await load('blacklight-exo-ecology-core.js');
+      await load('blacklight-exo-imagery-definitions.js');
       await load('blacklight-exo-imagery.js');
       await load('blacklight-exo-orbital-layout.js');
       await load('blacklight-exo-sol-campaign-data.js');
