770501c3710af2e7cbe13a4287fd557565b72843
mrcalzon02 <mrcalzon02@yahoo.com>
2026-08-09 14:01:05 -0800

    chore: advance vessel chronology cache seal

--- app-lite-view-mounts.js
+++ app-lite-view-mounts.js
@@ -225,7 +225,7 @@
       warhammerLogisticsPromise ||= loadScript('assets/warhammer-40k/imperial-logistics-v1.js?v=2');
       warhammerMercaturaPromise ||= Promise.all([warhammerLogisticsPromise, warhammerChronologyPromise]).then(() => loadScript('assets/warhammer-40k/imperial-mercatura-house-ledger-v1.js?v=2'));
       warhammerComponentLineagePromise ||= loadScript('assets/warhammer-40k/imperial-component-lineage-v1.js?v=2');
-      warhammerVesselHistoryPromise ||= loadScript('assets/warhammer-40k/imperial-vessel-history-v1.js?v=6');
+      warhammerVesselHistoryPromise ||= loadScript('assets/warhammer-40k/imperial-vessel-history-v1.js?v=7');
       warhammerPersonnelPromise ||= loadScript('assets/warhammer-40k/imperial-personnel-lineage-v1.js?v=5');
       warhammerSectorEventsPromise ||= loadScript('assets/warhammer-40k/imperial-sector-events-v1.js?v=4');
       warhammerMedicaeCatalogPromise ||= loadScript('assets/warhammer-40k/imperial-medicae-catalog-v1.js?v=1');
