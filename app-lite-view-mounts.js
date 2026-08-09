544ff5f548988524778c5cd1cf6d9a3235edf067
mrcalzon02 <mrcalzon02@yahoo.com>
2026-08-09 13:55:03 -0800

    chore: preload Cafarron chronology before Mercatura

--- app-lite-view-mounts.js
+++ app-lite-view-mounts.js
@@ -12,6 +12,7 @@
   let warhammerWorkspacePromise = null;
   let warhammerPlanetProfilePromise = null;
   let warhammerPlanetCompositorPromise = null;
+  let warhammerChronologyPromise = null;
   let warhammerLogisticsPromise = null;
   let warhammerMercaturaPromise = null;
   let warhammerComponentLineagePromise = null;
@@ -220,15 +221,16 @@
       warhammerLorePromise ||= loadScript('warhammer-40k-wiki-v6.js?v=6');
       warhammerPlanetProfilePromise ||= loadScript('assets/warhammer-40k/shaders/planet-profile-v1.js?v=8');
       warhammerPlanetCompositorPromise ||= warhammerPlanetProfilePromise.then(() => loadScript('assets/warhammer-40k/shaders/planet-compositor-v1.js?v=10'));
+      warhammerChronologyPromise ||= loadScript('assets/warhammer-40k/imperial-chronology-v1.js?v=1');
       warhammerLogisticsPromise ||= loadScript('assets/warhammer-40k/imperial-logistics-v1.js?v=2');
-      warhammerMercaturaPromise ||= warhammerLogisticsPromise.then(() => loadScript('assets/warhammer-40k/imperial-mercatura-house-ledger-v1.js?v=1'));
+      warhammerMercaturaPromise ||= Promise.all([warhammerLogisticsPromise, warhammerChronologyPromise]).then(() => loadScript('assets/warhammer-40k/imperial-mercatura-house-ledger-v1.js?v=2'));
       warhammerComponentLineagePromise ||= loadScript('assets/warhammer-40k/imperial-component-lineage-v1.js?v=2');
       warhammerVesselHistoryPromise ||= loadScript('assets/warhammer-40k/imperial-vessel-history-v1.js?v=6');
       warhammerPersonnelPromise ||= loadScript('assets/warhammer-40k/imperial-personnel-lineage-v1.js?v=5');
       warhammerSectorEventsPromise ||= loadScript('assets/warhammer-40k/imperial-sector-events-v1.js?v=4');
       warhammerMedicaeCatalogPromise ||= loadScript('assets/warhammer-40k/imperial-medicae-catalog-v1.js?v=1');
       warhammerMedicaePromise ||= warhammerMedicaeCatalogPromise.then(() => loadScript('assets/warhammer-40k/imperial-medicae-institutions-v1.js?v=3'));
-      await Promise.all([warhammerLorePromise, warhammerPlanetCompositorPromise, warhammerLogisticsPromise, warhammerMercaturaPromise, warhammerComponentLineagePromise, warhammerVesselHistoryPromise, warhammerPersonnelPromise, warhammerSectorEventsPromise, warhammerMedicaePromise]);
+      await Promise.all([warhammerLorePromise, warhammerPlanetCompositorPromise, warhammerChronologyPromise, warhammerLogisticsPromise, warhammerMercaturaPromise, warhammerComponentLineagePromise, warhammerVesselHistoryPromise, warhammerPersonnelPromise, warhammerSectorEventsPromise, warhammerMedicaePromise]);
       warhammerWorkspacePromise ||= loadScript('warhammer-40k-workspace-v8.js?v=31');
       await Promise.all([warhammerWorkspacePromise, base.prepareView(viewId)]);
       await window.Warhammer40KWorkspace?.initialize?.();
