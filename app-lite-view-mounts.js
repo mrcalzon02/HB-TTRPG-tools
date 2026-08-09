0ab9a669efbb2a6a6096290ff5ed19e8e955cf93
mrcalzon02 <mrcalzon02@yahoo.com>
2026-08-09 00:18:57 -0800

    chore: refresh Scientific Tools double slit entry

--- app-lite-view-mounts.js
+++ app-lite-view-mounts.js
@@ -203,7 +203,7 @@
     }
     if (viewId === 'scientific-tools') {
       ensureScientificToolsView();
-      scientificToolsEntryPromise ||= loadScript('scientific-tools-entry.js?v=20260809-ism-quantum-foam-1');
+      scientificToolsEntryPromise ||= loadScript('scientific-tools-entry.js?v=20260809-double-slit-3d-1');
       await scientificToolsEntryPromise;
       window.ScientificToolsWorkspace?.initialize?.();
       return;
