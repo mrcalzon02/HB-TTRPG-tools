730dac20b9680a7c6f7125bca19a4bf87e88c5d7
mrcalzon02 <mrcalzon02@yahoo.com>
2026-09-24 19:20:19 -0800

    Refresh customizable dice roller asset

--- app-lite-view-mounts.js
+++ app-lite-view-mounts.js
@@ -243,7 +243,7 @@
     }
     if (viewId === 'utilities') {
       sheetPromise ||= loadScript('character-sheet-view.js');
-      tabletopToolkitPromise ||= loadScript('tabletop-toolkit.js?v=20260924-dice3d-3');
+      tabletopToolkitPromise ||= loadScript('tabletop-toolkit.js?v=20260924-dice-custom-1');
       await Promise.all([sheetPromise, tabletopToolkitPromise]);
       base.initializeSheet();
       window.HBTabletopToolkit?.mount?.();
